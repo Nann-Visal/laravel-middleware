dd7a40203800d56077c43ed47f1e87f5328e66dd
Nann-Visal <visalnann@gmail.com>
2023-07-28 15:12:51 +0700

    first commit

---
+++ app/Http/Middleware/AfterMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AfterMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        $resonse = $next($request);
+
+        //  Perform action
+
+        return $resonse;
+    }
+}
